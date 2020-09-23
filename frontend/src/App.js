0efaf0d34627f04bee18863e8bb319fe60d175f8
Ben Conway <benrconway84@gmail.com>
2020-09-23 12:40:18 +0100

    Updating App to use header and main components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,13 @@
 import "./App.css";
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import { Home, About, SingleCake } from "./pages";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Header, Main } from "./components";
 
 export default function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/cakes/:id">
-            <SingleCake />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
+      <Header />
+      <Main />
     </Router>
   );
 }
--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders appropriate home screen in app component", () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/Home screen/i);
   expect(linkElement).toBeInTheDocument();
