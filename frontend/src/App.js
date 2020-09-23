9eda1decfd13b6b417e3bd5e4ec7a519ae7a2145
Ben Conway <benrconway84@gmail.com>
2020-09-23 19:45:39 +0100

    Added data fetching to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,21 @@ import "./App.css";
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Header, Main } from "./components";
+import { getData } from "./utils";
 
 export default function App() {
+  const [data, setData] = React.useState([]);
+
+  const loadData = async () => {
+    const res = await getData("/cakes");
+    setData(res);
+  };
+
+  React.useEffect(() => {
+    loadData();
+    return () => {};
+  }, []);
+
   return (
     <Router>
       <Header />
