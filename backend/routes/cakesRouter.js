de2cbb06d0e57caca24d7c08cb9ce950bcbbbf7c
Ben Conway <benrconway84@gmail.com>
2020-09-23 09:32:17 +0100

    Expanded functionality on cakes POST, PUT and DELETE functionality

--- backend/routes/cakesRouter.js
+++ backend/routes/cakesRouter.js
@@ -15,10 +15,24 @@ router.get("/:id", function (req, res) {
   res.send(selectedCake);
 });
 
-router.post("/", function (req, res) {});
+router.post("/", function (req, res) {
+  res.status(201).send(req.body);
+});
 
-router.put("/:id", function (req, res) {});
+router.put("/:id", function (req, res) {
+  const suppliedId = Number(req.params.id);
+  if (isNaN(suppliedId)) {
+    res.status(400).send("Sorry, something went terribly wrong");
+  }
+  res.status(200).send(req.body);
+});
 
-router.delete("/:id", function (req, res) {});
+router.delete("/:id", function (req, res) {
+  const suppliedId = Number(req.params.id);
+  if (isNaN(suppliedId)) {
+    res.status(400).send("Sorry, something went terribly wrong");
+  }
+  res.status(204).send();
+});
 
 module.exports = router;
