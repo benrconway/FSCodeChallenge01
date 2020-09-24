3ed99992be982b17fc96a6def1ca7cdbeff146ac
Ben Conway <benrconway84@gmail.com>
2020-09-24 07:05:58 +0100

    Updated cakeTile and CSS to display more info

--- frontend/src/components/cakeTile/cakeTile.js
+++ frontend/src/components/cakeTile/cakeTile.js
@@ -1,6 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function CakeTile({ cake }) {
-  const { name } = cake;
-  return <div>{`Cake is called ${name}`}</div>;
+  const { id, name, imageUrl } = cake;
+  console.log(cake);
+  return (
+    <div className="cake-tile">
+      <img className="cake-tile-image" src={imageUrl} alt={`${name}`} />
+      <p>{`${name}`}</p>
+      <Link to={`/cakes/${id}`}>More details</Link>
+    </div>
+  );
 }
