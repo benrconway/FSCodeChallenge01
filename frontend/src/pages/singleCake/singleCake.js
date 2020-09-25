de42b61379a4883f062b6df3914780a1c2c72d74
Ben Conway <benrconway84@gmail.com>
2020-09-25 20:51:12 +0100

    Updated SingleCake page and associated tests

--- frontend/src/pages/singleCake/singleCake.js
+++ frontend/src/pages/singleCake/singleCake.js
@@ -1,9 +1,34 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { getData } from "../../utils";
+import { CakeTile } from "../../components";
 
 export default function SingleCake() {
-  return (
-    <div>
-      <p>SingleCake screen</p>
-    </div>
-  );
+  const { id } = useParams();
+  const [cake, setCake] = React.useState([]);
+
+  const loadData = async () => {
+    const res = await getData(`/cakes/${id}`);
+    setCake(res);
+  };
+
+  const displayCake = (cake) => {
+    console.log(cake);
+    if (!cake || cake.name === undefined) {
+      return <p>Cake is still baking</p>;
+    }
+    return (
+      <CakeTile
+        cake={cake}
+        key={`${cake.name}-${cake.id}`}
+        fullDescription={true}
+      />
+    );
+  };
+
+  React.useEffect(() => {
+    loadData();
+  }, []);
+
+  return <div>{displayCake(cake)}</div>;
 }
--- frontend/src/pages/singleCake/singleCake.test.js
+++ frontend/src/pages/singleCake/singleCake.test.js
@@ -1,9 +1,27 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, waitForElement } from "@testing-library/react";
 import SingleCake from "./singleCake";
 
-test("renders single cake screen", () => {
-  const { getByText } = render(<SingleCake />);
-  const linkElement = getByText(/SingleCake screen/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock("../../utils", () => ({
+  getData: () => ({
+    id: 1,
+    name: "__TEST_CAKE__",
+    comment: "__TEST_COMMENT__",
+    imageUrl: "__TEST_URL__",
+    yumFactor: 5,
+  }),
+}));
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useParams: () => ({
+    id: 1,
+  }),
+  useRouteMatch: () => ({ url: "/cakes/1" }),
+}));
+
+test("renders single cake screen", async () => {
+  const { getByText, asFragment } = render(<SingleCake />);
+  const nameElement = await waitForElement(() => getByText(/__TEST_CAKE__/i));
+  expect(nameElement).toBeInTheDocument();
+  expect(asFragment()).toMatchSnapshot();
 });
