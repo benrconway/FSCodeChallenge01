354d287710cad7ba3317f839fbb3b5bcc9a5f222
Ben Conway <benrconway84@gmail.com>
2020-09-28 09:58:19 +0100

    Added New Cake page to router switch

--- frontend/src/components/main/main.js
+++ frontend/src/components/main/main.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import { Home, About, SingleCake } from "../../pages";
+import { Home, About, SingleCake, NewCake } from "../../pages";
 
 export default function Main({ data }) {
   return (
     <main>
       <Switch>
+        <Route path="/new">
+          <NewCake />
+        </Route>
         <Route path="/about">
           <About />
         </Route>
