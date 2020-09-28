4b9e1255ef3da67a65ad62296c2d9f7be7742e21
Ben Conway <benrconway84@gmail.com>
2020-09-28 09:57:42 +0100

    Corrected index.js import for addCakeForm

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,6 +1,6 @@
 import Header from "./header/header";
 import Main from "./main/main";
 import CakeTile from "./cakeTile/cakeTile";
-import addCakeForm from "./addCakeForm";
+import addCakeForm from "./addCakeForm/addCakeForm";
 
 export { Header, Main, CakeTile, addCakeForm };
