0da374944fcda39d7d8671b70c35b771613af9c8
Ben Conway <benrconway84@gmail.com>
2020-09-28 09:56:18 +0100

    Added New Cake Page

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,5 +1,6 @@
 import Home from "./home/home";
 import About from "./about/about";
 import SingleCake from "./singleCake/singleCake";
+import NewCake from "./newCake/newCake";
 
-export { Home, About, SingleCake };
+export { Home, About, SingleCake, NewCake };
---
+++ frontend/src/pages/newCake/newCake.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function newCakePage() {
+  return <div>New Cake Page</div>;
+}
