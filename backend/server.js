1e94360156da0b4a35fa33a6bf1d10a68519ac47
Ben Conway <benrconway84@gmail.com>
2020-09-29 10:05:28 +0100

    Added CORS handling for cross-origin of front end

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,17 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const cakesRouter = require("./routes/cakesRouter");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
 app.use("/cakes", cakesRouter);
 
